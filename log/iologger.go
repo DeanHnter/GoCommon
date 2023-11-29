44fe36d878b64eb141ac9f74cfe21cdf04ed4245
dh <46441813+DeanHnter@users.noreply.github.com>
2023-11-29 07:07:55 +0100

    Update iologger.go

--- log/iologger.go
+++ log/iologger.go
@@ -10,16 +10,16 @@ type IOLogger struct {
     Out io.Writer
 }
 
-func (bl *IOLogger) Log(level Level, message string) {
+func (bl IOLogger) Log(level Level, message string) {
     if level >= bl.Level {
         fmt.Fprintln(bl.Out, message)
     }
 }
 
-func (bl *IOLogger) SetOutput(out io.Writer) {
+func (bl IOLogger) SetOutput(out io.Writer) {
     bl.Out = out
 }
 
-func (bl *IOLogger) SetLevel(level Level) {
+func (bl IOLogger) SetLevel(level Level) {
     bl.Level = level
 }
